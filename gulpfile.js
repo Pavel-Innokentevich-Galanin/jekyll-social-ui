3fc7199fd188f0964c29a5cd46aa6cc9c6eeae5b
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2019-07-05 09:29:46 +0300

    beutifull gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,49 @@
 const
-    proxy = "127.0.0.1:4000",
-    HTML_files = [
-        "./**/*.html",
-        "!./**/node_modules/**/*.html",
-        "!./_site/**/*.html"
-    ],
-    SASS_flies = [
-        "./assets/sass/-var/**/*.sass",
-        "./assets/sass/**/*.sass"
-    ],
-    dist_SASS_files = "./uploads/";
+    SRC = {
+        HTML: [
+            './**/*.html',
+            '!./**/node_modules/**/*.html',
+            '!./**/_site/**/*.html'
+        ],
+        SASS: [
+            './_includes/constants/**/*.sass',
+            './_includes/**/*.sass'
+        ]
+    },
+    DIST = {
+        SASS: './uploads/' 
+    },
+    WATCH = {
+        HTML: SRC['HTML'],
+        SASS: SRC['SASS']
+    },
+    proxy = '127.0.0.1:4000';
 
 const
-    browserSync = require("browser-sync"),
-    gulp = require("gulp"),
+    browserSync  = require("browser-sync"),
+    gulp         = require("gulp"),
     autoprefixer = require("gulp-autoprefixer"),
-    clean_css = require("gulp-clean-css"),
-    concat = require("gulp-concat"),
-    notify = require("gulp-notify"),
-    sass = require("gulp-sass");
+    clean_css    = require("gulp-clean-css"),
+    concat       = require("gulp-concat"),
+    notify       = require("gulp-notify"),
+    sass         = require("gulp-sass");
 
 gulp.task('html', function () {
-    return gulp.src(HTML_files)
+    return gulp.src(SRC['HTML'])
         .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('sass', function () {
-    return gulp.src(SASS_flies)
+    return gulp.src(SRC['SASS'])
         .pipe(concat('style.sass'))
-        .pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
-        .pipe(concat('style.min.css'))
+        .pipe(sass({ outputStyle: 'expanded' })
+        .on("error", notify.onError()))
+        .pipe(concat('style.css'))
         .pipe(autoprefixer({
             browsers: ['last 300 versions']
         }))
         .pipe(clean_css())
-        .pipe(gulp.dest(dist_SASS_files))
+        .pipe(gulp.dest(DIST['SASS']))
         .pipe(browserSync.reload({ stream: true }));
 });
 
@@ -48,8 +57,8 @@ gulp.task('serve', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(HTML_files, gulp.parallel('html'));
-    gulp.watch(SASS_flies, gulp.parallel('sass'));
+    gulp.watch(WATCH['HTML'], gulp.parallel('html'));
+    gulp.watch(WATCH['SASS'], gulp.parallel('sass'));
 });
 
 gulp.task('default', gulp.parallel('watch', 'html', 'sass', 'serve'));
